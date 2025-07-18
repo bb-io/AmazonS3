cd74007691b66f40f65c5e448a9df3cc75622a6e
Alex Terehov <terales@users.noreply.github.com>
2025-07-18 12:00:52 -0230

    Add test for a folders data handler

--- Tests.AmazonS3/DataHandlerTests.cs
+++ Tests.AmazonS3/DataHandlerTests.cs
@@ -1,4 +1,5 @@
 using Apps.AmazonS3.DataSourceHandlers;
+using Apps.AmazonS3.Polling.Models;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Tests.AmazonS3.Base;
 
@@ -12,6 +13,21 @@ public class DataHandlerTests : TestBase
     {
         var handler = new BucketDataHandler(InvocationContext);
         var result = await handler.GetDataAsync(new DataSourceContext { }, CancellationToken.None);
+        Assert.IsNotNull(result);
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.Value}: {item.DisplayName}");
+        }
+    }
+
+    [TestMethod]
+    public async Task FolderDataHandler_returns_items()
+    {
+        var pollingFolderRequest = new PollingFolderRequest { BucketName = "myuniquebucketfortesting" };
+        var handler = new FolderDataHandler(InvocationContext, pollingFolderRequest);
+
+        var result = await handler.GetDataAsync(new DataSourceContext { }, CancellationToken.None);
+
         Assert.IsNotNull(result);
         foreach (var item in result)
         {
