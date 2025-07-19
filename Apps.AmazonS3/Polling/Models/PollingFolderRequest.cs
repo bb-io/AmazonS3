01ad2026b2f8e56eaae0c82ab2a6430556e2b061
Alex Terehov <terales@users.noreply.github.com>
2025-07-19 06:59:54 -0230

    Simplify 'On files updated' inputs

--- Apps.AmazonS3/Polling/Models/PollingFolderRequest.cs
+++ Apps.AmazonS3/Polling/Models/PollingFolderRequest.cs
@@ -1,12 +1,18 @@
+using Apps.AmazonS3.DataSourceHandlers;
 using Apps.AmazonS3.DataSourceHandlers.Static;
 using Apps.AmazonS3.Models.Request;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.AmazonS3.Polling.Models;
 
-public record PollingFolderRequest : FolderRequest
-{   
+public record PollingFolderRequest
+{
+    [Display("Folder ID (prefix)")]
+    [DataSource(typeof(FolderDataHandler))]
+    public string FolderId { get; set; } = string.Empty;
+
     [Display("Folder relation trigger")]
     [StaticDataSource(typeof(FolderRelationTriggerDataHandler))]
     public string? FolderRelationTrigger { get; set; }
